b1bac75bd781c72235f0bd577e4e24ab7ae34ad6
aezarebski <aezarebski@gmail.com>
2024-06-17 11:34:37 +1000

    plot more stuff

--- visualisation.py
+++ visualisation.py
@@ -68,6 +68,17 @@ tree_times_df = pd.DataFrame(
         for dd in data_dicts
     ]
 )
+
+cases_df = pd.DataFrame(
+    [
+        {
+            "key_num": dd["key_num"],
+            "prevalence": dd["prevalence"],
+            "cumulative_infections": dd["cumulative_infections"],
+        }
+        for dd in data_dicts
+    ]
+)
 # Setting up dataframes from the simulated data:1 ends here
 
 # [[file:vis.org::*Plot: random selection of R0 functions][Plot: random selection of R0 functions:1]]
@@ -145,4 +156,34 @@ last_seq_hist_p9 = (
 # Save the plot
 last_seq_hist_p9.save(f"{PLOT_DIR}/last_seq_hist.png", width=10, height=10, dpi=300)
 last_seq_hist_p9.save(f"{PLOT_DIR}/last_seq_hist.svg", width=10, height=10, dpi=300)
+
+
+prevalence_hist_p9 = (
+    p9.ggplot()
+    + p9.geom_histogram(
+        data=cases_df,
+        mapping=p9.aes(x="prevalence"),
+        bins=20,
+    )
+    + p9.scale_x_log10()
+    + p9.theme_bw()
+    + p9.theme(axis_title_y=p9.element_blank())
+)
+prevalence_hist_p9.save(f"{PLOT_DIR}/prevalence_hist.png", width=10, height=10, dpi=300)
+prevalence_hist_p9.save(f"{PLOT_DIR}/prevalence_hist.svg", width=10, height=10, dpi=300)
+
+
+cumulative_infections_hist_p9 = (
+    p9.ggplot()
+    + p9.geom_histogram(
+        data=cases_df,
+        mapping=p9.aes(x="cumulative_infections"),
+        bins=20,
+    )
+    + p9.scale_x_log10()
+    + p9.theme_bw()
+    + p9.theme(axis_title_y=p9.element_blank())
+)
+cumulative_infections_hist_p9.save(f"{PLOT_DIR}/cumulative_infections_hist.png", width=10, height=10, dpi=300)
+cumulative_infections_hist_p9.save(f"{PLOT_DIR}/cumulative_infections_hist.svg", width=10, height=10, dpi=300)
 # Distribution of last sequence times:1 ends here
